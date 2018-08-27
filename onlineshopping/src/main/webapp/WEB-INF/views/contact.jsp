e427b70f114c9f35f5f4b6bd7c608a15568afd5a
rajsky123 <bisen.rajiv000@gmail.com>
2018-08-28 04:48:10 +0530

    Active Menu solved

---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,12 @@
+<div class="container">
+
+	<div class="row">
+
+
+		<h1>About Us</h1>
+		<hr>
+		<h4>This is about page</h4>
+
+	</div>
+
+</div>
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function(){
+	//Solving active menu problem
+	
+	switch(menu){
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	
+	default:
+		$('#home').addClass('active');
+		break;
+	}
+	
+	
+});
