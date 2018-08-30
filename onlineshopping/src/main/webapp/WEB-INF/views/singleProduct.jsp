67464df5c643dfafc43d6e79d7f478e98a449b45
rajsky123 <bisen.rajiv000@gmail.com>
2018-08-31 03:02:41 +0530

    out of stock bug fixed

--- onlineshopping/src/main/webapp/WEB-INF/views/singleProduct.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/singleProduct.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <div class="container">
 
 	<!-- Breadcrumb -->
@@ -33,22 +34,65 @@
 		<!-- Display product description -->
 		<div class="col-xs-12 col-sm-8"></div>
 
-            <h3>${product.name}</h3>
-            <hr>
-            
-            <p>${product.description}</p>
-            <hr>
-            
-            <h4>Price:<strong> &#8377; ${product.unitPrice} /-</strong></h4>
-            <hr>
-            
-            <h6>Qty. Available: ${product.quantity}</h6>
-            
-            <a href="${contextRoot}/cart/add/${product.id}/product" class="btn btn-success">
-            	<span class="glyphicon glyphicon-shopping-cart"></span>Add To Cart</a>
-            
-			<a href="${contextRoot}/show/all/products" class="btn btn-primary">
-            	Back</a>
+		<h3>${product.name}</h3>
+		<hr>
+
+		<p>${product.description}</p>
+		<hr>
+
+		<h4>
+			Price:<strong> &#8377; ${product.unitPrice} /-</strong>
+		</h4>
+		<hr>
+
+
+
+		<c:choose>
+
+			<c:when test="${product.quantity<1}">
+
+				<h6>
+					Qty. Available: <span style="color: red">Out of Stock</span>
+				</h6>
+			</c:when>
+
+			<c:otherwise>
+
+				<h6>Qty. Available: ${product.quantity}</h6>
+
+
+			</c:otherwise>
+
+		</c:choose>
+
+
+		<c:choose>
+
+			<c:when test="${product.quantity<1}">
+
+				<a href="javascript:void(0)"
+					class="btn btn-success disabled"><del> <span
+					class="glyphicon glyphicon-shopping-cart" ></span>Add To 
+					Cart</del>
+				</a>
+			</c:when>
+
+			<c:otherwise>
+
+				<a href="${contextRoot}/cart/add/${product.id}/product"
+					class="btn btn-success"> <span
+					class="glyphicon glyphicon-shopping-cart"></span>Add To Cart
+				</a>
+
+
+			</c:otherwise>
+
+		</c:choose>
+
+
+
+		<a href="${contextRoot}/show/all/products" class="btn btn-primary">
+			Back</a>
 	</div>
 
 
--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,103 +1,116 @@
-$(function(){
-	//Solving active menu problem
-	
-	switch(menu){
+$(function() {
+	// Solving active menu problem
+
+	switch (menu) {
 	case 'Contact Us':
 		$('#contact').addClass('active');
 		break;
-	
+
 	case 'About Us':
 		$('#about').addClass('active');
 		break;
-	
+
 	case 'All Products':
 		$('#listProducts').addClass('active');
 		break;
 	default:
-		if(menu=='Home')break;
+		if (menu == 'Home')
+			break;
 		$('#listProducts').addClass('active');
-		$('#a_'+menu).addClass('active');
+		$('#a_' + menu).addClass('active');
 		break;
 	}
-	
-	
-	//code for datatable
 
-	
-	var $table=$('#productListTable');
-	
-	
-	//execute this code in table
-	
-	if($table.length)
-		{
-		
-		var jsonUrl='';
-		if(window.categoryId=='')
-			{
-			  jsonUrl=window.contextRoot+'/json/data/all/products';
-			}
-		else
-			{
-			jsonUrl=window.contextRoot+'/json/data/category/'+window.categoryId+'/products';
-			}
-		
-		
-		   $table.DataTable({
-			   lengthMenu: [[3,5,10,-1],['3 Records','5 Records','10 Records','All']],
-			   pageLength:5,
-			   ajax:{
-				   url:jsonUrl,
-				   dataSrc:''
-			   },
-			   columns:[
-				   
-				   {
-					    data:'code',
-					    bSortable:false,
-					    	mRender: function(data,type,row){
-						    	
-						    	return '<img src="'+window.contextRoot+'/resources/images/'+data+'.jpg" class="dataTableImg"/>';
-						    }	
-				   },    
-				   {
-				    data:'name'
-				   },
-				   
-				   {
-					  data:'brand'
-				   },
-				   
-				   {
-					    data:'unitPrice',
-					    mRender: function(data,type,row){
-					    	
-					    	return '&#8377; '+data;
-					    }	
-				   },
-				   
-				   {
-					    data:'quantity'
-				   },
-				   
-				   {
-					    data:'id',
-					    bSortable:false,
-					    	mRender: function(data,type,row){
-						    	var str='';
-						    	str+='<a href="'+window.contextRoot+'/show/'+data+'/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"/></a> &#160;';
-						    	str+='<a href="'+window.contextRoot+'/cart/add/'+data+'/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"/></a>';
-						    	return str;
-						    }
-				   }
-			          ]
-		   });
-			 
-		
+	// code for datatable
+
+	var $table = $('#productListTable');
+
+	// execute this code in table
+
+	if ($table.length) {
+
+		var jsonUrl = '';
+		if (window.categoryId == '') {
+			jsonUrl = window.contextRoot + '/json/data/all/products';
+		} else {
+			jsonUrl = window.contextRoot + '/json/data/category/'
+					+ window.categoryId + '/products';
 		}
-	
-	
-	
-	
-	
+
+		$table
+				.DataTable({
+					lengthMenu : [ [ 3, 5, 10, -1 ],
+							[ '3 Records', '5 Records', '10 Records', 'All' ] ],
+					pageLength : 5,
+					ajax : {
+						url : jsonUrl,
+						dataSrc : ''
+					},
+					columns : [
+
+							{
+								data : 'code',
+								bSortable : false,
+								mRender : function(data, type, row) {
+
+									return '<img src="' + window.contextRoot
+											+ '/resources/images/' + data
+											+ '.jpg" class="dataTableImg"/>';
+								}
+							},
+							{
+								data : 'name'
+							},
+
+							{
+								data : 'brand'
+							},
+
+							{
+								data : 'unitPrice',
+								mRender : function(data, type, row) {
+
+									return '&#8377; ' + data;
+								}
+
+							},
+
+							{
+								data : 'quantity',
+								mRender : function(data, type, row) {
+									if (data < 1)
+										return '<span style="color:red">Out Of Stock!</span>';
+
+									return data;
+								}
+
+							},
+
+							{
+								data : 'id',
+								bSortable : false,
+								mRender : function(data, type, row) {
+									var str = '';
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/show/'
+											+ data
+											+ '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"/></a> &#160;';
+
+									if (row.quantity < 1){
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"/></a>';
+									return str;
+									}
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/cart/add/'
+											+ data
+											+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"/></a>';
+									return str;
+								}
+							} ]
+				});
+
+	}
+
 });
